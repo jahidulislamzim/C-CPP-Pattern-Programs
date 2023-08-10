40ba15bc0826b51da37e48ca5f7f83869dcd1c02
jahidulislamzim <jahidulislamzim845@gmail.com>
2023-08-10 09:22:12 +0600

    adding 05

---
+++ C Patterns/pattern-05.c
@@ -0,0 +1,27 @@
+/*
+Here is the demo of pattern.
+5 5 5 5 5
+4 4 4 4 4
+3 3 3 3 3
+2 2 2 2 2
+1 1 1 1 1 
+*/
+
+
+#include <stdio.h>
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = n; j >=1; j--)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
